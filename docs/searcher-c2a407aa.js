37cc0308b5dd3fbd337f3ee843c2f67a2ea0e5e9
Eduardo Alves da Silva <eas@univali.br>
2026-08-31 20:50:21 -0300

    Updates

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-eb314bcd.js',
+                path_to_root + 'searchindex-535b33ed.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
